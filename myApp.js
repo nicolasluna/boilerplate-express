85f769f585e34df9ffbf642a0a29f3952ee023f4
nicolasluna <nicolasluna83@gmail.com>
2023-08-29 21:07:56 -0300

    add static content

--- myApp.js
+++ myApp.js
@@ -1,12 +1,17 @@
 let express = require('express');
 let app = express();
 
+//#4
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
 //#3
 app.get('/', (req, res) => {
  let absolutePath = __dirname + '/views/index.html';
  res.sendFile(absolutePath);
 })
+
+app.use();
 
 //#2
 // app.get('/', (req, res) => {
